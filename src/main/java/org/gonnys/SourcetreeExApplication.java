1011a81619c74be9376a18bdec62460b473255b6
Gonnys <gonnys520@gmail.com>
2018-11-08 17:36:40 +0900

    new repository

---
+++ src/main/java/org/gonnys/SourcetreeExApplication.java
@@ -0,0 +1,12 @@
+package org.gonnys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcetreeExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcetreeExApplication.class, args);
+	}
+}
---
+++ src/main/java/org/gonnys/controller/GonnysController.java
@@ -0,0 +1,15 @@
+package org.gonnys.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GonnysController {
+
+	public GonnysController gonnysController() {
+		
+		String gonnys = "goeun";
+		
+		return null;
+	}
+	
+}
